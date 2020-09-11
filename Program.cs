30e7a351b3d64d7573e7d8132e37993378517807
skulla <skull3227@gmail.com>
2020-09-11 09:47:11 -0700

    botnot

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace botnot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("its me U look For?");
+        }
+    }
+}
